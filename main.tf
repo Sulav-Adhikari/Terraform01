b22d89833e6086472ded773c0de179ae4a92321a
Sulav-Adhikari <sulav.adk.7@gmail.com>
2024-04-02 16:51:49 +0545

    Added depends on

--- main.tf
+++ main.tf
@@ -4,12 +4,14 @@ module "S3" {
 }
 
 module "cloudfront" {
+  depends_on = [ module.S3 ]
     source = "./modules/cloudfront"
 
                              
 }
 
 module "cloudflare" {
+
   source      = "./modules/cloudflare"
   domain_name = "sulav-adhikari.com.np"
   cloudfront_domain_name = module.cloudfront.cloudfront_domain
