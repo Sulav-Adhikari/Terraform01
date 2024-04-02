b32f0752e312a79b835a6f8b67b49c0d6dcdcd01
Sulav-Adhikari <sulav.adk.7@gmail.com>
2024-04-02 20:50:33 +0545

    Added github link in source

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 module "S3" {
-    source = "./modules/S3"
+    source = "git@github.com:Sulav-Adhikari/Terraform01.git//modules/S3"
   
 }
 
 module "cloudfront" {
   depends_on = [ module.S3 ]
-    source = "./modules/cloudfront"
+    source = "git@github.com:Sulav-Adhikari/Terraform01.git//modules/cloudfront"
 
                              
 }
 
 module "cloudflare" {
 
-  source      = "./modules/cloudflare"
+  source      = "git@github.com:Sulav-Adhikari/Terraform01.git//modules/cloudflare"
   domain_name = "sulav-adhikari.com.np"
   cloudfront_domain_name = module.cloudfront.cloudfront_domain
 
